fe1373d6dda914d1f13e43cda18394e9fc1d48f0
Houssine <ekhay1@outlook.com>
2024-09-11 12:57:37 -0400

    added the user schema in the user collection

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,28 @@
 const { Schema, model } = require("mongoose");
 const bcryp = require("bcrypt");
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  //   games:[
+  // {
+  // type:Schema.Types.ObjectId,
+  // ref:"Game",
+  //   }
+  // ]
+});
