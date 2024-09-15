1a4ccbb7c7f0fc6f6c6caf0716e64cb5efae14c1
Houssine <ekhay1@outlook.com>
2024-09-15 18:27:06 -0400

    updated the database name in connection of the database  to much the one that is created in atlas mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/footynet`
+  process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/footyNet`
 );
 
 module.exports = mongoose.connection;
