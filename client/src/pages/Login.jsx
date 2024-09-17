40ac2c3ac990b1c2f5954a89ac72a02803986ab8
JGuzman87 <jguzman913@gmail.com>
2024-09-16 22:15:58 -0400

    submit form logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,20 +1,52 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useMutation } from '@apollo/client';
-
+// import { useMutation } from '@apollo/client';
 
 import "./styles/login.css";
 
+function Login(props) {
+  const [formState, setFormState] = useState({ email: "", password: "" });
 
-function Login() {
+  //update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  //submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await Login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    //clear form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <main className="login-main">
-      <form className="form-login form-control">
+      <form className="form-login form-control" onSubmit={handleFormSubmit}>
         <label>Username:</label>
-        <input className="form-control" />
+        <input 
+        className="form-control"
+        placeholder="username"
+        
+         />
 
         <label>Password:</label>
         <input className="form-control"></input>
