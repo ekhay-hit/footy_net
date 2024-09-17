c73cf8eaa578841453da59d44e2297abec17434e
JGuzman87 <jguzman913@gmail.com>
2024-09-16 21:37:40 -0400

    import useState

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
+import { useMutation } from '@apollo/client';
+
+
 import "./styles/login.css";
+
+
 function Login() {
+
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  
   return (
     <main className="login-main">
       <form className="form-login form-control">
