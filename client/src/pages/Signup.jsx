b842b65bab1259d1b70edc5de05783eb844eb047
Houssine <ekhay1@outlook.com>
2024-09-22 16:51:41 -0400

    complete profile
     added the logic to the profile creation, when singup it will take the user to complete profile and the user can either upload a photo or skip till later, also if user click on profile picture it should take the user to updateProfile again in case need to update the profile picture
    
     if the user has a picture it will show the user profile if the user does not have a picture it will show a default one, if no one is loggedin it will show the logo of the site

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./styles/singup.css";
-import CompleteProfile from "../components/completeProfile";
 import { useMutation } from "@apollo/client";
-import { CREATE_USER, UPDATE_USER } from "../utils/mutations";
+import { CREATE_USER } from "../utils/mutations";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+
 function Signup() {
-  const [showSignup, setShowSignup] = useState(true);
-  const [showUpdateUser, setUpdateUser] = useState(true);
   // declairing state for signup input
   const [userFormData, setUserFormData] = useState({
     username: "",
@@ -14,12 +15,9 @@ function Signup() {
     password: "",
   });
 
-  const [avatar, setAvatar] = useState();
-
   const navigate = useNavigate();
   // add muations here
   const [createUser, { error }] = useMutation(CREATE_USER);
-  const [updateUser] = useMutation(UPDATE_USER);
   // handel signup input data
   const handleSignupInput = (event) => {
     const { name, value } = event.target;
@@ -31,91 +29,51 @@ function Signup() {
     event.preventDefault();
     try {
       const { data } = await createUser({ variables: { ...userFormData } });
-      setShowSignup(false);
-      setUpdateUser(true);
+
+      localStorage.setItem("isNewUser", "true"); // when user signup set a isNewUser to true to use to navigate to updateProfile instead of home
+      Auth.login(data.createUser.token);
+      navigate("/updateProfile");
+      // callign refetch from query me
     } catch (err) {
       console.log("failed to add user");
     }
   };
-
-  function skip() {
-    // if user skip navigate home
-    navigate("/");
-  }
-  const updateProfile = async (event) => {
-    event.preventDefault();
-    console.log("I am here in update the profile");
-    console.log(avatar);
-    try {
-      let avatarBase64 = null;
-      if (avatar) {
-        avatarBase64 = await new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onloadend = () => resolve(reader.result);
-          reader.onerror = (error) => reject(error);
-          reader.readAsDataURL(avatar);
-        });
-      }
-
-      const { data } = await updateUser({
-        variables: { avatar: avatarBase64 },
-      });
-      // after update navigate to home
-      navigate("/");
-    } catch (error) {
-      console.log("failed to update the user");
-      console.error(error);
-    }
-  };
   return (
-    <>
-      <main className="signup-main">
-        {showUpdateUser && (
-          <div className="complete-profile">
-            <CompleteProfile
-              setAvatar={setAvatar}
-              onUpload={updateProfile}
-              onSkip={skip}
-            />
-          </div>
-        )}
-        {showSignup && (
-          <form className="form form-control" onSubmit={handelSubmitForm}>
-            <label>Username:</label>
-            <input
-              type="text"
-              name="username"
-              placeholder="username"
-              value={userFormData.username}
-              onChange={handleSignupInput}
-              className="form-control"
-            />
+    <main className="signup-main">
+      <form className="form form-control" onSubmit={handelSubmitForm}>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={userFormData.username}
+          onChange={handleSignupInput}
+          className="form-control"
+        />
 
-            <label>email:</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="your email address"
-              value={userFormData.email}
-              onChange={handleSignupInput}
-              className="form-control"
-            />
+        <label>email:</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="your email address"
+          value={userFormData.email}
+          onChange={handleSignupInput}
+          className="form-control"
+        />
 
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="enter a password"
-              value={userFormData.password}
-              onChange={handleSignupInput}
-              className="form-control"
-            ></input>
-            {error && <div className="error">{error.message}</div>}
-            <button>Signup</button>
-          </form>
-        )}
-      </main>
-    </>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="enter a password"
+          value={userFormData.password}
+          onChange={handleSignupInput}
+          className="form-control"
+        ></input>
+        {error && <div className="error">{error.message}</div>}
+        <button>Signup</button>
+      </form>
+    </main>
   );
 }
 
